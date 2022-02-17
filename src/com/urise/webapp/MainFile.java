d6116f42d3c5c7dc3ec737766d0ab7bf56a78dcf
renegade <renegadethousand@gmail.com>
2022-02-17 21:32:27 +0300

    Lesson 8. File

---
+++ src/com/urise/webapp/MainFile.java
@@ -0,0 +1,32 @@
+package com.urise.webapp;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Objects;
+
+public class MainFile {
+    public static void main(String[] args) {
+        String filePath = ".\\.gitignore";
+        File file = new File(filePath);
+        try {
+            System.out.println(file.getCanonicalPath());
+        } catch (IOException e) {
+           throw new RuntimeException("Error", e);
+        }
+        File dir = new File("./src/com/urise/webapp");
+        System.out.println(dir.isDirectory());
+        for (String name : Objects.requireNonNull(dir.list())) {
+            System.out.println(name);
+        }
+        System.out.println(dir.list());
+
+
+        try (FileInputStream fis = new FileInputStream(filePath);){
+            System.out.println(fis.read());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        }
+}
