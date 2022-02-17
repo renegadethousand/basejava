c7145ac3c451ff7d1b312917051b601e81124586
renegade <renegadethousand@gmail.com>
2022-02-18 00:09:10 +0300

    iterate dir

--- src/com/urise/webapp/MainFile.java
+++ src/com/urise/webapp/MainFile.java
@@ -8,25 +8,36 @@ import java.util.Objects;
 
 public class MainFile {
     public static void main(String[] args) {
-        String filePath = ".\\.gitignore";
+        iterateDir("./src");
+//        String filePath = ".\\.gitignore";
+//        File file = new File(filePath);
+//        try {
+//            System.out.println(file.getCanonicalPath());
+//        } catch (IOException e) {
+//            throw new RuntimeException("Error", e);
+//        }
+//        File dir = new File("./src/com/urise/webapp");
+//        System.out.println(dir.isDirectory());
+//        for (String name : Objects.requireNonNull(dir.list())) {
+//            System.out.println(name);
+//        }
+//        System.out.println(dir.list());
+//
+//
+//        try (FileInputStream fis = new FileInputStream(filePath);) {
+//            System.out.println(fis.read());
+//        } catch (IOException e) {
+//            throw new RuntimeException(e);
+//        }
+    }
+
+    public static void iterateDir(String filePath) {
         File file = new File(filePath);
-        try {
-            System.out.println(file.getCanonicalPath());
-        } catch (IOException e) {
-           throw new RuntimeException("Error", e);
+        System.out.println(file.getName());
+        if (file.isDirectory()) {
+            for (File iter : file.listFiles()) {
+                iterateDir(iter.getAbsolutePath());
+            }
         }
-        File dir = new File("./src/com/urise/webapp");
-        System.out.println(dir.isDirectory());
-        for (String name : Objects.requireNonNull(dir.list())) {
-            System.out.println(name);
-        }
-        System.out.println(dir.list());
-
-
-        try (FileInputStream fis = new FileInputStream(filePath);){
-            System.out.println(fis.read());
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-        }
+    }
 }
