3442b312a3e2ef6d9162d26836a633422a719494
renegade <renegadethousand@gmail.com>
2022-03-09 22:49:43 +0300

    Lesson 10. Multithreading

---
+++ src/com/urise/webapp/MainConcurrency.java
@@ -0,0 +1,56 @@
+package com.urise.webapp;
+
+import com.sun.tools.javac.Main;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainConcurrency {
+
+    public static final int THREADS_NUMBER = 10_000;
+    private static int counter;
+
+    private static final Object LOCK = new Object();
+
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println(Thread.currentThread().getName());
+        Thread thread = new Thread() {
+            @Override
+            public void run() {
+                System.out.println(getName() + ", " + getState());
+//                throw new IllegalStateException();
+            }
+        };
+        thread.start();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println(Thread.currentThread().getName() + ", " + Thread.currentThread().getState());
+            }
+        }).start();
+        System.out.println(thread.getState());
+
+        final MainConcurrency mainConcurrency = new MainConcurrency();
+        List<Thread> threads = new ArrayList<>(THREADS_NUMBER);
+
+        for (int i = 0; i < THREADS_NUMBER; i++) {
+            Thread thread1 = new Thread(() -> {
+                for (int j = 0; j < 100; j++) {
+                    mainConcurrency.inc();
+                }
+            });
+            thread1.start();
+            threads.add(thread);
+        }
+
+        for (Thread thread1 : threads) {
+            thread1.join();
+        }
+
+        System.out.println(counter);
+    }
+
+    private synchronized void inc() {
+        counter++;
+    }
+}
