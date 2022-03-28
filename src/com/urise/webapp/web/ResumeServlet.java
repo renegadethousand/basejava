706357c14ee24b949ff98702c26eb159457cc997
renegade <renegadethousand@gmail.com>
2022-03-28 23:45:37 +0300

    Lesson 16. JSP

--- src/com/urise/webapp/web/ResumeServlet.java
+++ src/com/urise/webapp/web/ResumeServlet.java
@@ -10,7 +10,6 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
-import java.io.Writer;
 
 public class ResumeServlet extends HttpServlet {
 
@@ -22,37 +21,9 @@ public class ResumeServlet extends HttpServlet {
         storage = (SqlStorage) Config.get().getSqlStorage();
     }
 
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        request.setCharacterEncoding("UTF-8");
-        response.setCharacterEncoding("UTF-8");
-
-        response.setContentType("text/html; charset=UTF-8");
-        Writer writer = response.getWriter();
-        writer.write(
-                "<html>\n" +
-                        "<head>\n" +
-                        "    <meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\">\n" +
-                        "    <link rel=\"stylesheet\" href=\"css/style.css\">\n" +
-                        "    <title>Список всех резюме</title>\n" +
-                        "</head>\n" +
-                        "<body>\n" +
-                        "<section>\n" +
-                        "<table border=\"1\" cellpadding=\"8\" cellspacing=\"0\">\n" +
-                        "    <tr>\n" +
-                        "        <th>Имя</th>\n" +
-                        "        <th>Email</th>\n" +
-                        "    </tr>\n");
-        for (Resume resume : storage.getAllSorted()) {
-            writer.write(
-                    "<tr>\n" +
-                            "     <td><a href=\"resume?uuid=" + resume.getUuid() + "\">" + resume.getFullName() + "</a></td>\n" +
-                            "     <td>" + resume.getContact(ContactType.MAIL) + "</td>\n" +
-                            "</tr>\n");
-        }
-        writer.write("</table>\n" +
-                "</section>\n" +
-                "</body>\n" +
-                "</html>\n");
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        request.setAttribute("resumes", storage.getAllSorted());
+        request.getRequestDispatcher("/WEB-INF/jsp/list.jsp").forward(request, response);
     }
 
     @Override
---
+++ web/WEB-INF/jsp/list.jsp
@@ -0,0 +1,37 @@
+<%@ page import="com.urise.webapp.model.Resume" %>
+<%@ page import="java.util.List" %>
+<%@ page import="com.urise.webapp.model.ContactType" %><%--
+  Created by IntelliJ IDEA.
+  User: Джабраил
+  Date: 28.03.2022
+  Time: 23:31
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Список всех резюме</title>
+</head>
+<body>
+<section>
+    <table border="1" cellpadding="8" cellspacing="0">
+        <tr>
+            <th>Имя</th>
+            <th>Email</th>
+        </tr>
+        <%
+            for (Resume resume : (List<Resume>) request.getAttribute("resumes")) {
+        %>
+        <tr>
+            <td><a href="resume?uuid=<%=resume.getUuid()%>"><%=resume.getFullName()%></td></a>
+            <td><%=resume.getContact(ContactType.MAIL)%></td>
+        </tr>
+        <%
+            }
+        %>
+    </table>
+</section>
+</body>
+</html>
